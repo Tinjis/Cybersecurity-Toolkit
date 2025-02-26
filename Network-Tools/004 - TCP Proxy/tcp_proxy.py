5b5186c9160d6bba0e6d565c07b38b78a9b89b74
Tinjis <thecybertrojan@gmail.com>
2025-02-26 16:19:58 +0000

    Update tcp_proxy.py

--- Network-Tools/004 - TCP Proxy/tcp_proxy.py
+++ Network-Tools/004 - TCP Proxy/tcp_proxy.py
@@ -3,5 +3,17 @@ import socket
 import threading
 
 
-HEX_FILTER = ''.join([(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
+HEX_FILTER = ""
 
+for i in range(256):
+    char = chr(i)
+
+    if len(repr(char)) == 3:
+        HEX_FILTER += char
+
+    else:
+        HEX_FILTER += "."
+
+def hexdump(src, length=16, show=True):
+    if isinstance(src, bytes):
+        src = src.decode()
