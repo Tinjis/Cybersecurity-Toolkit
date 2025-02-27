ec170c6ceb6f9896d7c2011aa346f4cc32075988
Tinjis <thecybertrojan@gmail.com>
2025-02-27 15:43:08 +0000

    Update tcp_proxy.py

--- Network-Tools/004 - TCP Proxy/tcp_proxy.py
+++ Network-Tools/004 - TCP Proxy/tcp_proxy.py
@@ -35,3 +35,72 @@ def hexdump(src, length=16, show=True):
             print(line)
     else:
         return results
+    
+
+def receive_from(connection):
+    buffer = b""
+    connection.settimeout(5)
+
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+    
+    except Exception as e:
+        pass
+
+    return buffer
+
+
+def request_handler(buffer):
+    
+    return buffer
+
+def response_handler(buffer):
+
+    return buffer
+
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    remote_host.connect((remote_host, remote_port))
+
+    if receive_first:
+        remote_buffer = receive_from(remote_socket)
+        hexdump(remote_buffer)
+
+    remote_buffer = response_handler(remote_buffer)
+
+    if len(remote_buffer):
+        print(f"[->] Sending {len(remote_buffer)} to localhost")
+        client_socket.send(remote_buffer)
+
+    while True:
+
+        local_buffer = receive_from(client_socket)
+
+        if len(local_buffer):
+            print(f"[->]Received {local_buffer} bytes from localhost.")
+            hexdump(local_buffer)
+            local_buffer = request_handler(local_buffer)
+            remote_socket.send(local_buffer)
+            print("[->] Sent to remote.")
+
+        remote_buffer = receive_from(remote_socket)
+        if len(remote_buffer):
+            print("[<==] Received %d bytes from remote." % len(remote_buffer))
+            hexdump(remote_buffer)
+            remote_buffer = response_handler(remote_buffer)
+            client_socket.send(remote_buffer)
+            print("[->] Sent to localhost.")
+
+        if not len(local_buffer) or not len(remote_buffer):
+            client_socket.close()
+            remote_socket.close()
+            
+            print("[*] No more data. Closing connections.")
+            break
+
