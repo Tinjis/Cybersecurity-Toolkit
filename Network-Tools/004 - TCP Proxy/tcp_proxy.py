21c98f2573f4691cb9c6ad2d999b7fcb6e1c9ed1
Tinjis <thecybertrojan@gmail.com>
2025-02-28 11:23:40 +0000

    Update tcp_proxy.py

--- Network-Tools/004 - TCP Proxy/tcp_proxy.py
+++ Network-Tools/004 - TCP Proxy/tcp_proxy.py
@@ -25,7 +25,6 @@ def hexdump(src, length=16, show=True):
 
         printable = word.translate(HEX_FILTER)
         hexa = ' '.join([f'{ord(c):02X}' for c in word])
-
         hexwidth = length * 3
         
         results.append(f"{i:04x} {hexa:<{hexwidth}} {printable}")
@@ -66,13 +65,12 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
 
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    remote_host.connect((remote_host, remote_port))
+    remote_socket.connect((remote_host, remote_port))
 
     if receive_first:
         remote_buffer = receive_from(remote_socket)
         hexdump(remote_buffer)
-
-    remote_buffer = response_handler(remote_buffer)
+        remote_buffer = response_handler(remote_buffer)
 
     if len(remote_buffer):
         print(f"[->] Sending {len(remote_buffer)} to localhost")
@@ -83,7 +81,7 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
         local_buffer = receive_from(client_socket)
 
         if len(local_buffer):
-            print(f"[->]Received {local_buffer} bytes from localhost.")
+            print(f"[->] Received {local_buffer} bytes from localhost.")
             hexdump(local_buffer)
             local_buffer = request_handler(local_buffer)
             remote_socket.send(local_buffer)
@@ -91,7 +89,7 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
 
         remote_buffer = receive_from(remote_socket)
         if len(remote_buffer):
-            print("[<==] Received %d bytes from remote." % len(remote_buffer))
+            print(f"[->] Received {len(remote_buffer)} bytes from remote.")
             hexdump(remote_buffer)
             remote_buffer = response_handler(remote_buffer)
             client_socket.send(remote_buffer)
@@ -104,3 +102,6 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             print("[*] No more data. Closing connections.")
             break
 
+def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
