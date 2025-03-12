c4cd750d503312f48dad4aa01375406ef70fe3ca
Tinjis <thecybertrojan@gmail.com>
2025-03-12 21:28:25 +0000

    still working on ssh_server.py...

--- Network-Tools/006 - Paramiko SSH Tools/ssh_server.py
+++ Network-Tools/006 - Paramiko SSH Tools/ssh_server.py
@@ -18,3 +18,35 @@ class Server (paramiko.ServerInterface):
             return paramiko.OPEN_SUCCEEDED
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
     
+    def check_auth_password(self, username, password):
+        if (username == "username") and (password == "password"):
+            return paramiko.AUTH_SUCCESSFUL
+        
+
+if __name__ == "__main__":
+    server = "192.168.1.9"
+    ssh_port = 2222
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((server, ssh_port))
+        sock.listen(100)
+
+        print("[+] Listening for connection ...")
+        client, addr = sock.accept()
+    except Exception as e:
+        print(f"[X] Listen failed: {str(e)}")
+        sys.exit(1)
+    else:
+        print(f"[+] Got a connection! {client}, {addr}")            
+    
+    bhsession = paramiko.Transport(client)
+    bhsession.add_server_key(HOSTKEY)
+    server = Server()
+    bhsession.start_server(server=server)
+    chan = bhsession.accept(20)
+    if chan is None:
+        print("[X] No Channel.")
+        sys.exit(1)
+
+    
