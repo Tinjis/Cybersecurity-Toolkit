f054723cfd84dab1f4c4aed425cfa17f1b2ccfbb
cybertrojan <thecybertrojan@gmail.com>
2025-05-13 21:46:37 +0100

    Update sniffer.py

--- 001-Network-Tools/002-sniffing/002-detailed-packet-logger/sniffer.py
+++ 001-Network-Tools/002-sniffing/002-detailed-packet-logger/sniffer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from scapy.all import sniff, Ether, IP, TCP, UDP
-import time
 
 protocols = {6: "TCP", 17: "UDP", 1: "ICMP"}
 
@@ -21,15 +20,20 @@ def packet_processing(packet):
     elif packet.haslayer(UDP):
         udp = packet[UDP]
         print(f"[UDP] Src Port: {udp.sport} -> Dst Port: {udp.dport}")
+
     
 def main():
-    print("[*] Starting the packet sniffer", end="")
-    for i in range(0, 3):
-        print(".", end="")
-        time.sleep(1)
-    print("")
-    
-    sniff(prn=packet_processing, store=False)
 
+    duration = int(input("Enter sniffing time in seconds: "))
+    choice = input("Filter only TCP packets? (Yes/No): ").strip().lower()
+
+    print("[*] Starting the packet sniffer...")
+
+    if choice == "yes":
+        sniff(prn=packet_processing, store=False, timeout=duration, filter="tcp")
+    else:
+        sniff(prn=packet_processing, store=False, timeout=duration)
+    
+    
 if __name__ == "__main__":
     main()
